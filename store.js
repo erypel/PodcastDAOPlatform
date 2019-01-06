7b0f7758a33028dbefe7edf15f96666c85bc4c84
Evan <evan.rypel@gmail.com>
2019-01-06 13:27:04 -0800

    working through a hackernoon tutorial to build create user and login functionality

---
+++ store.js
@@ -0,0 +1,9 @@
+/**
+ * http://usejsdoc.org/
+ */
+module.exports = {
+		createUser ({username, password}) {
+			 console.log(`Add user ${username} with password ${password}`)
+			    return Promise.resolve()
+		}
+}
