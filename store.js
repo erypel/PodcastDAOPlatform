cab64bfca4c1c1d4a6382b5d249cbc9538fa021a
Evan <evan.rypel@gmail.com>
2019-01-06 15:05:01 -0800

    encrypted passwords

--- store.js
+++ store.js
@@ -1,15 +1,34 @@
 /**
  * http://usejsdoc.org/
  */
+const crypto = require('crypto')
 const knex = require('knex')(require('./knexfile'))
 
 module.exports = {
+		saltHashPassword,
 		createUser ({username, email, password}) {
-			 console.log(`Add user ${username} with password ${password}`)
+			 console.log(`Add user ${username}`)
+			 const {salt, hash} = saltHashPassword(password)
 			 return knex('user').insert({
 				 username,
 				 email,
-				 password
+				 salt,
+				 encrypted_password: hash
 			 })
 		}
+}
+
+// we want to store encrypted passwords in the DB
+function saltHashPassword(password){
+	const salt = randomString()
+	const hash = crypto.createHmac('sha512', salt).update(password)
+	
+	return {
+		salt,
+		hash: hash.digest('hex')
+	}
+}
+
+function randomString(){
+	return crypto.randomBytes(4).toString('hex')
 }
