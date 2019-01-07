fc417efcc559c062e066ef38b07ec843040383ce
Evan <evan.rypel@gmail.com>
2019-01-07 11:49:41 -0800

    added logout button and session middleware

--- app.js
+++ app.js
@@ -15,8 +15,33 @@ app.use(session({
 	cookieName: 'session',
 	secret: store.randomString(),
 	duration: 30 * 60 * 1000,//in millis, 30 minutes for now, probably want more
-	activeDuration: 5 * 60 * 1000 //add 5 more minutes of session time if user interacts with page
+	activeDuration: 5 * 60 * 1000, //add 5 more minutes of session time if user interacts with page
+	httpOnly: true, //prevent browser js from accessing cookies
+	secure: true, //ensures cookies are only used over HTTPS
+	ephemeral: true //deletes cookie when browser is closed
 }))
+
+//Session middleware
+app.use(function(req, res, next) {
+	//check that a session exists
+	if(req.session && req.session.user){
+		//TODO look for user in DB
+		/*
+		 * if user is in db
+		 * 		req.user = user;
+		 * 		delete req.user.password //delete password from the session
+		 * 		req.session.user = user //refresh session value
+		 * 
+		 * 		
+		 */
+		//finish processing the middleware and run the route
+		req.user = req.session.user
+		delete req.user.password
+		next()
+	} else {
+		next()
+	}
+})
 
 
 app.get('/', function(req, res) {
@@ -27,28 +52,34 @@ app.get('/login', function(req, res) {
 	res.render("login")
 })
 
+app.get('/logout', function(req, res){
+	req.session.reset()
+	res.redirect('/')
+})
+
 app.get('/createUser', function(req, res) {
 	res.render("createUser")
 })
 
-app.get('/dashboard', function(req, res) {
-	//check that a session exists
-	if(req.session && req.session.user){
-		//TODO look for user in DB
-		/*
-		 * if user is not in db
-		 * 		req.session.reset()
-		 * 		res.redirect('/login')
-		 * else
-		 * 		//render
-		 * 		res.render("dashboard")
-		 * 		
-		 */
-		res.render('dashboard')
-	} else {
-		res.redirect('login')
-	}
-	
+/**
+ * check if a user is logged in and redirect them if they're not
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+function requireLogin (req, res, next) {
+	console.log("login required")
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+}
+
+app.get('/dashboard', requireLogin, function(req, res) {
+	//TODO bug where user logs out and can navigate back to dashboard with back button
+	res.render('dashboard')
 })
 
 app.post('/createUser', (req, res) => {
