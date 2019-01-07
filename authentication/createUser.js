da403864ad4b9cc13b9246a96b6d1f36468a1d79
Evan <evan.rypel@gmail.com>
2019-01-06 19:54:11 -0800

    initial dashboard commit

--- authentication/createUser.js
+++ authentication/createUser.js
@@ -9,14 +9,3 @@ CreateUser.addEventListener('submit', (e) => {
   const password = CreateUser.querySelector('.password').value
   post('/createUser', { username, email, password })
 })
-
-function post (path, data) {
-  return window.fetch(path, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-}
