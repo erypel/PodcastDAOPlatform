d6977a482ee06d51a858c5d46cfa76193676d9b4
Evan <evan.rypel@gmail.com>
2019-01-26 13:46:36 -0800

    consolidated some code

--- wallet/routes.js
+++ wallet/routes.js
@@ -19,42 +19,34 @@ function getFunds(ownerID) {
 	return  walletStore.getUserBalance(ownerID)
 }
 
+function renderWallet(userID, res, destinationTag = null){
+	let walletID = Promise.resolve(getWalletID(userID))
+	walletID.then(function(value){
+		walletID = value
+	})
+	let funds = Promise.resolve(getFunds(userID))
+	funds.then(function(value){
+		funds = value
+		console.log("userID: " + userID + " walletID: " + walletID + " funds: " + funds)
+		res.render('wallet', {userID, walletID, funds, destinationTag})
+	})
+}
+
 router.get('/mapDestinationTag', session.requireLogin, (req, res) => {
 	// use userID to map to destination tag
 	let userID = req.session.user.id
 	let destinationTag = Promise.resolve(walletMapper.map(userID))
 	destinationTag.then(function(value){
 		destinationTag = value
-	})
-	
-	// duplicate code of get /wallet
-	//TODO consolidate 
-	let walletID = Promise.resolve(getWalletID(userID))
-	walletID.then(function(value){
-		walletID = value
-	})
-	let funds = Promise.resolve(getFunds(userID))
-	funds.then(function(value){
-		funds = value
-		console.log("userID: " + userID + " walletID: " + walletID + " funds: " + funds)
-		res.render('wallet', {userID, walletID, funds, destinationTag})
+		renderWallet(userID, res, destinationTag)
 	})
 })
 
 router.get('/wallet', session.requireLogin, (req, res) => {
-	//TODO add dest tag stuff
 	let destinationTag = ''
 	let userID = req.session.user.id
-	let walletID = Promise.resolve(getWalletID(userID))
-	walletID.then(function(value){
-		walletID = value
-	})
-	let funds = Promise.resolve(getFunds(userID))
-	funds.then(function(value){
-		funds = value
-		console.log("userID: " + userID + " walletID: " + walletID + " funds: " + funds)
-		res.render('wallet', {userID, walletID, funds, destinationTag})
-	})
+	
+	renderWallet(userID, res)
 })
 
 module.exports = router
