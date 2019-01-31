cbc592bd6cada86643fd04feab46e6e28475e85e
Evan <evan.rypel@gmail.com>
2019-01-31 13:31:46 -0800

    advertisement dropdown is dynamically populated with teh ads created by the logged in user

--- advertising/adcampaign/routes.js
+++ advertising/adcampaign/routes.js
@@ -8,7 +8,24 @@ const session = require('../../authentication/session')
 const adStore = require('../adStore')
 
 router.get('/campaign', session.requireLogin, (req, res) => {
-	res.render('campaign')
+	let userID = req.session.user.id
+	adStore.selectAdsByUserID(userID).then((ads) => {
+		let dropdown = []
+		let itemsProcessed = 0
+		ads.forEach(function(el){
+			let ad = {}
+			ad.name = el.ad_name
+			ad.id = el.id
+			dropdown.push(ad)
+			itemsProcessed++
+			if(itemsProcessed === ads.length){
+				console.log('dropdown', dropdown)
+				res.render('campaign', {
+					ads: dropdown
+				})
+			}
+		})
+	})
 })
 
 module.exports = router
