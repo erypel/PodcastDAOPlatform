f71491bf9cffe9af77f072f95713fefbd408bf97
Evan <evan.rypel@gmail.com>
2019-02-15 13:26:35 -0800

    configured logger a little bit

--- app.js
+++ app.js
@@ -4,6 +4,25 @@
  */
 const express = require('express')
 const app = express()
+const { createLogger, format, transports } = require('winston')
+
+// This will log to console. Eventually will want a log file
+const logger = createLogger({
+	// levels are:
+	// { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+	level: 'debug',
+	format: format.combine(
+		    format.colorize(),
+		    format.timestamp({
+		      format: 'YYYY-MM-DD HH:mm:ss'
+		    }),
+		    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+		  ),
+	transports: [new transports.Console()]
+})
+
+logger.info('Logging is up and running!')
+
 app.set('view engine', 'ejs');
 
 //Static audio files
