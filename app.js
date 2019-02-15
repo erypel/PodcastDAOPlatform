2276e2a0117cd882aaff05121159086529cf22e1
Evan <evan.rypel@gmail.com>
2019-02-15 13:38:47 -0800

    logger is now its own module

--- app.js
+++ app.js
@@ -4,24 +4,9 @@
  */
 const express = require('express')
 const app = express()
-const { createLogger, format, transports } = require('winston')
+const logger = require('./logger')(__filename)
 
-// This will log to console. Eventually will want a log file
-const logger = createLogger({
-	// levels are:
-	// { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
-	level: 'debug',
-	format: format.combine(
-		    format.colorize(),
-		    format.timestamp({
-		      format: 'YYYY-MM-DD HH:mm:ss'
-		    }),
-		    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-		  ),
-	transports: [new transports.Console()]
-})
-
-logger.info('Logging is up and running!')
+logger.info('Logger is initialized.')
 
 app.set('view engine', 'ejs');
 
