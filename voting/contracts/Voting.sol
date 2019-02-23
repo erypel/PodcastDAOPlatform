2852f13d05d5f1d27773fa94a0a69dd5ff33f75f
Evan <evan.rypel@gmail.com>
2019-02-23 15:44:04 -0800

    configured first contract so that it could work

--- voting/contracts/Voting.sol
+++ voting/contracts/Voting.sol
@@ -1,41 +1,69 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.5.0;
 
 contract Voting {
+	// An event that is called whenever a Candidate is added so the frontend can
+	// appropriately display the candidate with the right element ID (it is used
+	// to vote for the candidate, since it is one of the args for the function vote()
 	event AddedCandidate(uint candidateID);
 	
+	// describes a Voter, which has an ID and the ID of the candidate they voted for
 	struct Voter {
 		bytes32 uid;
 		uint candidateIDVote;
 	}
 	
+	// describes a Candidate
 	struct Candidate {
 		bytes32 name;
 		bytes32 party;
+		
+		// checks if this Struct exists
+		// lets us keep track of candidates
 		bool doesExist;
 	}
 	
-	uint numCandidates;
+	// These state variables are used keep track of the number of Candidates/Voters 
+    // and used to as a way to index them   
+	uint numCandidates; 
 	uint numVoters;
 	
+	// Think of these as a hash table, with the key as a uint and value of 
+    // the struct Candidate/Voter. These mappings will be used in the majority
+    // of our transactions/calls
+    // These mappings will hold all the candidates and Voters respectively
 	mapping (uint => Candidate) candidates;
 	mapping (uint => Voter) voters;
 	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     *  These functions perform transactions, editing the mappings *
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+	
 	function addCandidate(bytes32 name, bytes32 party) public {
+		// candidateID is the return variable
 		uint candidateID = numCandidates++;
+		// Create new Candidate Struct with name and saves it to storage.
 		candidates[candidateID] = Candidate(name, party, true);
-		AddedCandidate(candidateID);
+		emit AddedCandidate(candidateID);
 	}
 	
 	function vote(bytes32 uid, uint candidateID) public {
+		// checks if the struct exists for that candidate
 		if(candidates[candidateID].doesExist == true) {
-			uint voterID = numVoters++;
+			uint voterID = numVoters++; //voterID is the return variable
 			voters[voterID] = Voter(uid, candidateID);
 		}
 	}
 	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * 
+     *  Getter Functions, marked by the key word "view" *
+     * * * * * * * * * * * * * * * * * * * * * * * * * */
+	
+	// finds the total amount of votes for a specific candidate by looping
+    // through voters 
 	function totalVotes(uint candidateID) view public returns (uint) {
-		uint numOfVotes = 0;
+		uint numOfVotes = 0; // we will return this
 		for(uint i = 0; i < numVoters; i++) {
+			// if the voter votes for this specific candidate, we increment the number
 			if(voters[i].candidateIDVote == candidateID) {
 				numOfVotes++;
 			}
@@ -47,6 +75,7 @@ contract Voting {
 		return numVoters;
 	}
 	
+	// returns candidate information, including its ID, name, and party
 	function getCandidate(uint candidateID) public view returns (uint, bytes32, bytes32) {
 		return (candidateID, candidates[candidateID].name, candidates[candidateID].party);
 	}
--- voting/truffle.js
+++ voting/truffle.js
@@ -1,3 +1,17 @@
 /**
  * The truffle config file
+ * 
+ * from `project directory`\voting, run `truffle.cmd develop` to start truffle
+ * run `compile` to compile it to bytecode first for the EVM to execute
  */
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*" // Match any network id
+    }
+  }
+}
